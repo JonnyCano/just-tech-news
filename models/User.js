8c9d8df460a501fbe87531aba8b6c7946a92f613
Jonathan Cano <jcano528@gmail.com>
2021-10-07 01:02:50 -0500

    added a sequelize tableName attribute to the User model to solve the naming issue from the recent develop branch

--- models/User.js
+++ models/User.js
@@ -54,7 +54,8 @@ User.init(
     // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
     underscored: true,
     // make it so our model name stays lowercase in the database
-    modelName: 'user'
+    modelName: 'user',
+    tableName: 'user'
     }
 );
 
