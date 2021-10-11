33ea2f0f692575216c1aceae41165e6a69b9cdad
Jonathan Cano <jcano528@gmail.com>
2021-10-10 19:12:05 -0500

    installed express-session library to connect to the backend and connect-session-sequelize libraryto store the sessions created by express-session into the just tech news database. Created the forms to login and register on the app. Fetched to the api routes by leveraging asynch/await for cleaner code.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+
 const hbs = exphbs.create({});
 const path = require('path');
 const routes = require('./controllers/');
@@ -7,6 +9,20 @@ const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
